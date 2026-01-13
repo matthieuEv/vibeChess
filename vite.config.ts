6b3ae01053dceaf80954a2fb38cc73b15559bd8f
matthieuEv <evainmatthieu44@gmail.com>
2026-01-13 09:18:21 +0100

    fix: fix type check

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-    exclude: ['e2e/**'],
-  },
   base: './',
   server: {
     headers: {
